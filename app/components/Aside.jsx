01c5a9b3be3cc88b9009d264c14a43b7a2429c42
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2023-12-21 15:24:56 +0100

    feat aside v2

--- app/components/Aside.jsx
+++ app/components/Aside.jsx
@@ -5,11 +5,14 @@ function Aside() {
 
     return (
         <aside className="aside-section">
-            <img className="logo" src={logo.src} ></img>
-            <div className="element__group">
-                <ul className="navigation">
-                    <li className="item"></li>
-                </ul>
+            <div className="content">
+                <img className="logo" src={logo.src} ></img>
+                <div className="element__group">
+                    <ul className="navigation">
+                        <li className="item">Test</li>
+                        <li className="item">Test</li>
+                    </ul>
+                </div>
             </div>
         </aside>
     )
