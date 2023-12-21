98f8de9a2d55aceb37d34ed7c39eea403a3fcbd3
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2023-12-21 15:00:58 +0100

    logo aside

--- app/components/Aside.jsx
+++ app/components/Aside.jsx
@@ -1,10 +1,11 @@
 
+import logo from '../assets/images/logo-peugeot.svg';
 
 function Aside() {
 
     return (
         <aside className="aside-section">
-            <img className="logo" src=""></img>
+            <img className="logo" src={logo.src} ></img>
             <div className="element__group">
                 <ul className="navigation">
                     <li className="item"></li>
